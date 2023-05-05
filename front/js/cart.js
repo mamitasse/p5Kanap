a56fcf7f357bdb23ce442923d900030d72705373
Tassadit Idiri <mamantasse@gmail.com>
2023-05-05 17:30:38 +0200

    final

--- front/js/cart.js
+++ front/js/cart.js
@@ -65,11 +65,9 @@ const createArticle = () => {
         </div>
         </article>
       `;
-      
       section.appendChild(article);
       sumPrices += element.product.price * element.quantity;
       sumQuantity += element.quantity;
-      
     });
   }
   price.innerText = sumPrices.toFixed(2);
@@ -127,45 +125,154 @@ const createArticle = () => {
       /* On met à jour l'affichage de la somme totale du prix */
       price.innerText = sumPrices.toFixed(2);
       quantity.innerText = sumQuantity;
-
-      
     });
-    
   }
-  
+  const deleteButtons = document.getElementsByClassName("deleteItem");
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", function() {
+      function removeArticleFromLocalStorage(id, color) {
+        let cart = JSON.parse(localStorage.getItem("cart"));
+        if (!cart) {
+          return;
+        }
+        let updatedCart = cart.filter((item) => {
+          return !(item.id === id && item.color === color);
+        });
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
+      }
+    });
+  }
 };
 
 createArticle(); // Appel de la fonction pour afficher les articles du panier
 
-// Récupérer les données du panier depuis le localStorage
-let cart = JSON.parse(localStorage.getItem("cart"));
+/// Message envoyé si l’entrée est vide lors de l’envoi du formulaire
+function inputEmpty(errorMsg, input) {
+  errorMsg.innerHTML = 'Veuillez renseigner ce champ.';
+  input.classList.add('showInputError')
+  setTimeout(() => {
+    errorMsg.innerHTML = '';
+    input.classList.remove('showInputError')
+  }, 3000);
+};
 
-// Fonction pour supprimer un article du panier
-function deleteArticle(productId, color) {
-  // Trouver l'article correspondant
-  let index = cart.findIndex((article) => article.id === productId && article.color === color);
+// Message envoyé si la valeur d’entrée n’est pas correcte
+const errorMessage = {
+  name: 'Minimum 2 caractères, maximum 20 caractères. Les majuscules, minuscules et tirets (-) sont autorisés.',
+  address: 'Minimum 5 caractères, maximum 50 caractères. Les chiffres, lettres et tirets (-) sont autorisés.',
+  city: 'Minimum 2 caractères, maximum 20 caractères. Les majuscules, minuscules et tirets (-) sont autorisés.',
+  email: 'Veuillez renseigner une adresse mail valide.'
+};
 
-  // Supprimer l'article s'il est trouvé
-  if (index !== -1) {
-    cart.splice(index, 1);
+function inputNotValid(input, errorMsg, msg) {
+  errorMsg.innerHTML = msg;
+  input.classList.add('showInputError');
+  setTimeout(() => {
+    errorMsg.innerHTML = '';
+    input.classList.remove('showInputError')
+  }, 8000);
+};
+
+document.querySelector('.cart__order__form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formValue = {
+    firstName: document.querySelector('#firstName').value,
+    lastName: document.querySelector('#lastName').value,
+    address: document.querySelector('#address').value,
+    city: document.querySelector('#city').value,
+    email: document.querySelector('#email').value
+  };
+
+  const inputValidation = {
+    firstName: false,
+    lastName: false,
+    address: false,
+    city: false,
+    email: false
+  };
+
+  const nameRegex = /^[A-Za-z-]{2,20}$/;
+  const addressRegex = /^[A-Za-z0-9\s-]{2,50}$/;
+  const cityRegex = /^[A-Za-z-]{2,20}$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (formValue.firstName === '') {
+    inputEmpty(document.querySelector('#firstNameErrorMsg'), document.querySelector('#firstName'));
+  } else if (!nameRegex.test(formValue.firstName)) {
+    inputNotValid(document.querySelector('#firstName'), document.querySelector('#firstNameErrorMsg'), errorMessage.name);
+  } else {
+    inputValidation.firstName = true;
+  };
+
+  if (formValue.lastName === '') {
+    inputEmpty(document.querySelector('#lastNameErrorMsg'), document.querySelector('#lastName'));
+  } else if (!nameRegex.test(formValue.lastName)) {
+    inputNotValid(document.querySelector('#lastName'), document.querySelector('#lastNameErrorMsg'), errorMessage.name);
+  } else {
+    inputValidation.lastName = true;
+  };
+
+  if (formValue.address === '') {
+    inputEmpty(document.querySelector('#addressErrorMsg'), document.querySelector('#address'));
+  } else if (!addressRegex.test(formValue.address)) {
+    inputNotValid(document.querySelector('#address'), document.querySelector('#addressErrorMsg'), errorMessage.address);
+  } else {
+    inputValidation.address = true;
+  };
+
+  if (formValue.city === '') {
+    inputEmpty(document.querySelector('#cityErrorMsg'), document.querySelector('#city'));
+  } else if (!cityRegex.test(formValue.city)) {
+    inputNotValid(document.querySelector('#city'), document.querySelector('#cityErrorMsg'), errorMessage.city);
+  } else {
+    inputValidation.city = true;
+  };
+
+
+  if(formValue.email == ''){
+      inputEmpty(document.querySelector('#emailErrorMsg'), document.querySelector('#email'));
+  } else if(!emailRegex.test(formValue.email)){
+      inputNotValid(document.querySelector('#email'), document.querySelector('#emailErrorMsg'), errorMessage[0].email);  
+  } else {
+      inputValidation.email = true;
+  };
+
+  if(inputValidation.firstName && inputValidation.lastName &&  inputValidation.address && inputValidation.city && inputValidation.email){
+   
+      sendForm(formValue);
+      console.log(formValue);
+  }
+}); 
+
+//Envoyer les informations des produits panier et formulaire au serveur pour récupérer l’ID de command
+function sendForm(formValue){
+
+  let productInStorage = getCart();
+  let products = [];
+  for(let product of productInStorage){
+      products.push(product.id)
   }
 
-  // Mettre à jour le panier dans le localStorage
-  localStorage.setItem("cart", JSON.stringify(cart));
+  let contact = formValue;
 
-  // Mettre à jour l'affichage du panier
-  createArticle();
-}
+  const sendFormData = {
+      contact,
+      products,
+    }
 
-// Ajouter un gestionnaire d'événements pour le bouton "Supprimer"
-const deleteButtons = document.getElementsByClassName("deleteItem");
-for (let i = 0; i < deleteButtons.length; i++) {
-  deleteButtons[i].addEventListener("click", function () {
-    let article = this.closest("article");
-    let productId = article.getAttribute("data-id");
-    let color = article.getAttribute("data-color");
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(sendFormData),
+      headers: { 
+        'Content-Type': 'application/json',
+      }
+    };
 
-    // Appeler la fonction de suppression avec les bons arguments
-    deleteArticle(productId, color);
+  fetch("http://localhost:3000/api/products/order", options)
+      .then(response => response.json())
+          .then(data => {
+              document.location.href =`confirmation.html?id=${data.orderId}`;
+              localStorage.clear();
   });
-}
+};  
